442a92aba92648e2eaea9dfd443be900083e0ccf
chris imoni <imonireal@gmail.com>
2023-06-15 22:56:15 +0100

    setup kafka consumer

---
+++ src/main/java/com/chrisimoni/libraryeventsproducer/config/KafkaConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.chrisimoni.libraryeventsproducer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+}
