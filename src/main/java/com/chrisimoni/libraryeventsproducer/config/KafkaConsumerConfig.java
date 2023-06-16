a7719c9753981e490f91e61aa05d39cf4ec6f95a
chris imoni <imonireal@gmail.com>
2023-06-16 18:46:36 +0100

    added config for parallel consumer processing and a sample class for manual offset commit

--- src/main/java/com/chrisimoni/libraryeventsproducer/config/KafkaConsumerConfig.java
+++ src/main/java/com/chrisimoni/libraryeventsproducer/config/KafkaConsumerConfig.java
@@ -1,9 +1,31 @@
 package com.chrisimoni.libraryeventsproducer.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+import java.util.List;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class KafkaConsumerConfig {
+
+    //config for needed for manually handling or committing the offset, concurrency consumer and etc
+    //also for scaling application in parallel
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, kafkaConsumerFactory);
+        factory.setConcurrency(3);
+        return factory;
+    }
+
+
 }
