9fef9e5b226b1d2be37b0ab0364cb853900384a1
chris imoni <imonireal@gmail.com>
2023-07-11 22:01:19 +0100

    added new topic configurations

--- src/main/java/com/chrisimoni/libraryeventsproducer/config/KafkaConfig.java
+++ src/main/java/com/chrisimoni/libraryeventsproducer/config/KafkaConfig.java
@@ -10,10 +10,20 @@ import org.springframework.kafka.config.TopicBuilder;
 public class KafkaConfig {
     @Value("${spring.kafka.topic}")
     public String topic;
+    @Value("${spring.kafka.new-event-topic}")
+    public String newLibraryEventTopic;
 
     @Bean
     public NewTopic libraryEvents() {
         return TopicBuilder.name(topic)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic newLibraryEvents() {
+        return TopicBuilder.name(newLibraryEventTopic)
                 .partitions(3)
                 .replicas(1)
                 .build();
